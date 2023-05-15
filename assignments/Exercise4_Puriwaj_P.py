1648585048cd891ff0d46e223c6b325effd76264
Puriwaj <103435359+Puriwaj@users.noreply.github.com>
2023-05-15 16:50:11 +0700

    Add files via upload
    Exercise 4

---
+++ assignments/Exercise4_Puriwaj_P.py
@@ -0,0 +1,20 @@
+'''
+This is a score result program.
+
+There are 4 subjects include
+1. FE = Foundation English
+2. GB = General Business
+3. ICS = Introduction to Computer Systems
+4. CP = Computer Programming
+'''
+###############
+FE = 98
+GB = 84.5
+ICS = 96
+CP = 75.5
+###############
+print('--- Your Score ---')
+print('Foundation English :',FE)
+print('General Business : ',GB)
+print('Introduction to Computer Systems : ',ICS)
+print('Computer Programming : ',CP)
