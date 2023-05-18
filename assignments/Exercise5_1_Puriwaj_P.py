28819d95f46b1e7890bb3ab85bab18d2ba71e031
Puriwaj <103435359+Puriwaj@users.noreply.github.com>
2023-05-18 13:12:30 +0700

    Add files via upload

---
+++ assignments/Exercise5_1_Puriwaj_P.py
@@ -0,0 +1,15 @@
+print('----------Calculator----------')
+firstDiction = float(input('firstDiction  : '))
+secondDiction = float(input('secondDiction : '))
+print('------------------------------')
+########################################
+plus = firstDiction + secondDiction
+minus = firstDiction - secondDiction
+multiply = firstDiction * secondDiction
+divide = firstDiction / secondDiction
+########################################
+print(firstDiction,'+',secondDiction,'=',plus)
+print(firstDiction,'-',secondDiction,'=',minus)
+print(firstDiction,'*',secondDiction,'=',multiply)
+print(firstDiction,'/',secondDiction,'=',divide)
+print('------------------------------')
---
+++ assignments/Exercise5_2_Puriwaj_P.py
@@ -0,0 +1,9 @@
+print('---Velocity Calculator---')
+s = float(input('distance [m] : '))
+t = float(input('time [s]     : '))
+print('-------------------------')
+############
+v = s/t
+############
+print('velocity [m/s] =',v)
+print('-------------------------')
